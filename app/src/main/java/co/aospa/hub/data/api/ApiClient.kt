e778e4328cafb16226ec1b2125f0ecb5850579c9
Hernán Castañón Álvarez <herna@paranoidandroid.co>
2024-02-13 12:56:33 +0100

    Enable network logging fo Debug mode.
    Signed-off-by: Hernán Castañón Álvarez <herna@paranoidandroid.co>

--- app/src/main/java/co/aospa/hub/data/api/ApiClient.kt
+++ app/src/main/java/co/aospa/hub/data/api/ApiClient.kt
@@ -1,10 +1,15 @@
 package co.aospa.hub.data.api
 
+import co.aospa.hub.BuildConfig
 import co.aospa.hub.GlobalConstants
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.okhttp.OkHttp
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.plugins.logging.SIMPLE
 import io.ktor.client.request.get
 import io.ktor.client.statement.HttpResponse
 import io.ktor.http.ContentType
@@ -20,6 +25,12 @@ object ApiClient {
             json(
                 contentType = ContentType.Text.Any
             )
+        }
+        if (BuildConfig.DEBUG) {
+            install(Logging) {
+                logger = Logger.SIMPLE
+                level = LogLevel.ALL
+            }
         }
         defaultRequest {
             url {
